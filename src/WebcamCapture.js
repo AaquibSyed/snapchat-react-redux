fd0241f62d4952106f481920199553563877ed6e
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-01-12 20:58:51 +0530

    added redux feature to  capture image

--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,5 +1,8 @@
-import React, { useRef } from "react";
+import React, { useRef, useCallback } from "react";
 import Webcam from "react-webcam";
+import RadioButtonUncheckedIcon from "@material-ui/icons/RadioButtonUnchecked";
+import { useDispatch } from "react-redux";
+import { setImage } from "./features/camSlice";
 
 const videoConstaints = {
   height: 400,
@@ -9,6 +12,12 @@ const videoConstaints = {
 
 const WebcamCapture = () => {
   const webCamRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const capture = useCallback(() => {
+    const imageSource = webCamRef.current.getScreenshot();
+    dispatch(setImage(imageSource));
+  }, [webCamRef]);
 
   return (
     <div className="webcamCapture">
@@ -19,7 +28,12 @@ const WebcamCapture = () => {
         videoConstraints={videoConstaints}
         screenshotFormat="image/jpeg"
         ref={webCamRef}
-      ></Webcam>
+      />
+      <RadioButtonUncheckedIcon
+        className="webcamCapture__captureButton"
+        fontSize="large"
+        onClick={capture}
+      />
     </div>
   );
 };
---
+++ src/features/camSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const camSlice = createSlice({
+  name: "camSlice",
+  initialState: {
+    image: null,
+  },
+  reducers: {
+    setImage: (state, action) => {
+      state.image = action.payload;
+    },
+    resetImage: (state) => {
+      state.image = null;
+    },
+  },
+});
+
+export const { setImage, resetImage } = camSlice.actions;
+export const selectImage = (state) => state.camSlice.image;
+export default camSlice.reducer;
