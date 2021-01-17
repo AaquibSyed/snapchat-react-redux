c5195c63a6a1c9f69667472eb7cd662204b9a2cb
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-01-17 21:50:08 +0530

    chat header done

--- src/Chats.js
+++ src/Chats.js
@@ -1,9 +1,23 @@
 import "./Chats.css";
 
 import React from "react";
+import { Avatar } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
 
 function Chats() {
-  return <div className="chats">Chatscreen</div>;
+  return (
+    <div className="chats">
+      <div className="chats__header">
+        <Avatar className="chats__avatar" />
+        <div className="chats__search">
+          <SearchIcon className="chats__searchIcon" />
+          <input type="text" placeholder="friends" />
+        </div>
+        <ChatBubbleIcon className="chats__bubble" />
+      </div>
+    </div>
+  );
 }
 
 export default Chats;
