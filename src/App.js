a1685aeab72b20b575f5c11a928d6c411c415273
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-01-19 03:04:36 +0530

    login done@AaquibSyed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import WebcamCapture from "./WebcamCapture";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Preview from "./Preview";
 import Chats from "./Chats";
 import ChatView from "./ChatView";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUSer } from "./features/appSlice";
+import Login from "./Login";
+import { auth } from "./firebase";
 
 function App() {
+  const user = useSelector(selectUSer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            username: authUser.displayName,
+            profilePic: authUser.photoURL,
+            id: authUser.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <Router>
-        <div className="app__body">
-          <Switch>
-            <Route path="/preview">
-              <Preview />
-            </Route>
-            <Route path="/chats" exact>
-              <Chats />
-            </Route>
-            <Route path="/chats/view" exact>
-              <ChatView />
-            </Route>
-            <Route path="/" exact>
-              <WebcamCapture />
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="app__body">
+            <Switch>
+              <Route path="/preview">
+                <Preview />
+              </Route>
+              <Route path="/chats" exact>
+                <Chats />
+              </Route>
+              <Route path="/chats/view" exact>
+                <ChatView />
+              </Route>
+              <Route path="/" exact>
+                <WebcamCapture />
+              </Route>
+            </Switch>
+          </div>
+        )}
       </Router>
     </div>
   );
