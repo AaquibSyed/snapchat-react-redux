e11a4db314ae7d1c8fb83482464a8d28ca79c35e
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-01-18 19:45:49 +0530

    bug fix on click of viewed chat

--- src/Chat.js
+++ src/Chat.js
@@ -11,10 +11,6 @@ function Chat({ id, profilePic, username, imageUrl, timestamp, read }) {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const exit = () => {
-    history.replace("/");
-  };
-
   const open = () => {
     if (!read) {
       dispatch(selectImage(imageUrl));
@@ -27,17 +23,25 @@ function Chat({ id, profilePic, username, imageUrl, timestamp, read }) {
         }
       );
       history.push("/chats/view");
-    } else exit();
+    }
   };
   return (
     <div className="chat" onClick={open}>
       <Avatar src={profilePic} className="chat__avatar" />
       <div className="chat__info">
         <h4>{username}</h4>
-        <p>
-          Tap to view -
-          <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()} />
-        </p>
+        {!read && (
+          <p>
+            Tap to view -
+            <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()} />
+          </p>
+        )}
+        {read && (
+          <p>
+            Received{" "}
+            <ReactTimeago date={new Date(timestamp?.toDate()).toUTCString()} />
+          </p>
+        )}
       </div>
       {!read && <StopIcon className="chat__readIcon" />}
     </div>
